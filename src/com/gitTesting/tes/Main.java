bfbd4944028b27d4112f6a77af7bd3fa2407e858
SK REDOYAN AHMED <rafsanahmed02@gmail.com>
2025-11-17 16:59:52 -0500

    first attempt

---
+++ src/com/gitTesting/tes/Main.java
@@ -0,0 +1,11 @@
+package com.gitTesting.tes;
+
+    public class Main {
+        public static void main (String[] args) {
+
+            System.out.println("this is a git test");
+
+        }
+
+
+}
