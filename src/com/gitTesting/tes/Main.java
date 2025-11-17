304fba5826dd4e781682940eddd746720cf9b68e
SK REDOYAN AHMED <rafsanahmed02@gmail.com>
2025-11-17 17:01:41 -0500

    2nd attempt

--- src/com/gitTesting/tes/Main.java
+++ src/com/gitTesting/tes/Main.java
@@ -3,7 +3,7 @@ package com.gitTesting.tes;
     public class Main {
         public static void main (String[] args) {
 
-            System.out.println("this is a git test");
+            System.out.println("this is a git test2 ");
 
         }
 
